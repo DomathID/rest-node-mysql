de03eff7b8781d50d8a37a31e00e2b38dc67b814
バング ドマス <bangdompret@gmail.com>
2021-06-18 19:48:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
  const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '1234',
+  password: '',
   database: 'anime_db'
 });
 conn.connect((err) =>{
